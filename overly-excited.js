6ea9abf07aead96dc8fda5f53b9b5b804d39662a
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-03-05 21:02:51 -0600

    deleted commented out exercise instructions

--- overly-excited.js
+++ overly-excited.js
@@ -1,14 +1,8 @@
 console.log("hello overly excited");
 
-// Create an array that contains the words in the sentence
 let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
 let nextSentence = ["Now", "Melissa", "will", "write", "a", "magical", "unicorn", "sentence", "for", "this", "exercise"];
-let renderDiv = document.getElementById("render");
-/*
-    The addExcitement function should be an impure function, and accept
-    the array as the sole argument. It should iterate over the array
-    and output the words to the browser console.
-*/
+
 
 let addExcitement = (taco, punct) => {
     var joined = "";
@@ -16,7 +10,6 @@ let addExcitement = (taco, punct) => {
         joined += ` ${taco[i]}`;
         if(([i]+1)%3 === 0){
             joined += punct.repeat((i+1)/3);
-            // console.log("add exclaim" + joined);
         };   
     console.log("sentence: ", joined)
     };
